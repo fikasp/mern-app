4b2ad45dfba370b4a70c3052db8f73c6e2080570
fikasp <fikasp@gmail.com>
2023-07-13 06:54:35 +0200

    add concurrently

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,16 @@ import axios from 'axios'
 
 export default function App() {
 
+	const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000'
+	console.log(apiUrl)
+
 	useEffect( () => {
-		axios.get('http://localhost:5000/api')
+		axios.get(`${apiUrl}/api`)
 			.then(res => console.log(res.data.api))
 			.catch(error => {
 				console.log(error)
 			});
-	},[])
+	},[apiUrl])
 
 	return (
 		<div className='container'>
