4dc234fb2a2ff23170a0826936bfd31acb47804c
fikasp <fikasp@gmail.com>
2023-07-18 17:52:09 +0200

    add jest test

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -1,10 +1,17 @@
-import { expect } from 'chai'
-import request from 'supertest'
-import app from '../server.js'
+import app from '../server.js';
+import request from 'supertest';
 
-describe('GET /', () => {
-  it('should return status 200', async () => {
-    const response = await request(app).get('/')
-    expect(response.status).to.equal(200)
-  })
-})
+describe('GET /api/:id', () => {
+  it('should return status 200 and the record data if the record exists', async () => {
+    const recordId = 1
+    const response = await request(app).get(`/api/${recordId}`)
+    expect(response.status).toBe(200)
+    expect(response.body).toBeDefined()
+  });
+
+  it('should return status 404 if the record does not exist', async () => {
+    const nonExistentRecordId = 999
+    const response = await request(app).get(`/api/${nonExistentRecordId}`)
+    expect(response.status).toBe(404)
+  });
+});
