5729f11bf056b6216da9a9b3f6eddf10eecb6ba6
fikasp <fikasp@gmail.com>
2024-05-14 19:54:42 +0200

    fix vercel

--- api/config/config.js
+++ api/config/config.js
@@ -1,4 +1,4 @@
 export const CONNECTION_URL =
-	process.env.CONNECTION_URL
+	process.env.CONNECTION_URL || 'mongodb+srv://fikasp:fikasp@mern.cgw5ule.mongodb.net/mern?retryWrites=true&w=majority'
 	
 export const PORT = process.env.PORT || 5000
