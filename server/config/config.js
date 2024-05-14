493b682609829ba84ac287558611f1b73af58243
fikasp <fikasp@gmail.com>
2024-05-14 17:49:45 +0200

    fix connection url

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,5 @@
-export const CONNECTION_URL =	'mongodb+srv://fikasp:fikasp@mern.cgw5ule.mongodb.net/mern?retryWrites=true&w=majority'
+export const CONNECTION_URL =
+	process.env.CONNECTION_URL ||
+	'mongodb+srv://fikasp:fikasp@mern.cgw5ule.mongodb.net/mern?retryWrites=true&w=majority'
 	
 export const PORT = process.env.PORT || 5000
